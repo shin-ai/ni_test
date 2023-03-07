b6054a799fb403be7750c0b365a615d14783ff04
faldibule <faldibule@gmail.com>
2023-03-07 11:20:34 +0700

    Add: git ignore, cofig copy

---
+++ app/config/config copy.php
@@ -0,0 +1,9 @@
+<?php
+
+define('BASEURL', 'http://localhost/ni_test/public');
+
+//db
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'db_ni_test');
