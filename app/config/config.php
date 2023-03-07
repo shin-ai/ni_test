9fa1f562f4955eb2e0b3bd64c036ad5da8b63a09
faldibule <faldibule@gmail.com>
2023-03-07 11:22:54 +0700

    Delete: git config

--- app/config/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-define('BASEURL', 'http://localhost/job/Fanny/ni_test/public');
-
-//db
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'db_ni_test');
